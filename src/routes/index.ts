8398bd1286948e3096d14c647fc157d84bda152c
kayu <muhammadrizky6354@gmail.com>
2026-04-16 11:39:53 +0700

    tamabhan cors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import authRoutes from "./auth.route.js"
 import activityRoutes from "./activity.route.js"
 import productRoutes from "./product.routes.js";
 
 const routes = new Hono()
+
+routes.use(cors({
+  origin: 'https://hima-ti-uniku.vercel.app',
+  credentials: true,
+}))
 
 routes.route('/auth', authRoutes);
 routes.route('/activity', activityRoutes);
